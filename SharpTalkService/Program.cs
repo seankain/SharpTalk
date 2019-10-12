630bd3e63df165b70856a6f84d6a8f12a3680726
unknown <sean.kain.geo@gmail.com>
2019-10-12 12:13:44 -0400

    used Fleck websocket server and included as submodule, simple server and client transaction

--- SharpTalkService/Program.cs
+++ SharpTalkService/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Fleck;
+using SharpTalk;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.WebSockets;
@@ -15,30 +17,50 @@ namespace SharpTalkService
             if (args.Length < 1 || args[0] == "listen")
             {
                 Console.WriteLine("Listening");
-                using (var ws = new ClientWebSocket())
-                {
-                    ws.ConnectAsync(new Uri("ws://127.0.0.1"), CancellationToken.None);
-                    while (ws.State == WebSocketState.Open)
-                    {
-                        string msg = "aeiou";
-                        ArraySegment<byte> bytesToSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(msg));
-                         ws.SendAsync(bytesToSend, WebSocketMessageType.Text, true, CancellationToken.None).RunSynchronously();
-                        ArraySegment<byte> bytesReceived = new ArraySegment<byte>(new byte[1024]);
-                        WebSocketReceiveResult result = ws.ReceiveAsync(bytesReceived, CancellationToken.None).Result;
-                        var backMsg = Encoding.UTF8.GetString(bytesReceived.Array, 0, result.Count);
-                        Console.WriteLine(backMsg);
-                        //Console.WriteLine(Encoding.UTF8.GetString(bytesReceived.Array, 0, result.Count));
-
-                    }
-                }
+                Listen().Wait();
             }
             else
             {
                 Console.WriteLine("Hosting");
-                // PipeServer.Go();
-                var wss = new WebSocketServer(9009);
-                wss.Start();
+                using (var tts = new FonixTalkEngine())
+                {
+                    var server = new WebSocketServer("ws://127.0.0.1:8181");
+                    server.Start(socket =>
+                    {
+                        socket.OnOpen = () => Console.WriteLine("Open!");
+                        socket.OnClose = () => Console.WriteLine("Close!");
+                        socket.OnMessage = message => tts.Speak(message);
+                    });
+                    while (true)
+                    {
+                        if (Console.ReadKey() == new ConsoleKeyInfo('q', ConsoleKey.Q, false, false, false))
+                        {
+                            return;
+                        }
+                    }
+                }
             }
         }
+
+        private static async Task Listen() {
+            using (var ws = new ClientWebSocket())
+            {
+               await ws.ConnectAsync(new Uri("ws://127.0.0.1:8181"), CancellationToken.None);
+                while (ws.State == WebSocketState.Open)
+                {
+                    //string msg = "aeiou";
+                    var msg = Console.ReadLine();
+                    ArraySegment<byte> bytesToSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(msg));
+                    await ws.SendAsync(bytesToSend, WebSocketMessageType.Text, true, CancellationToken.None);
+                    //ArraySegment<byte> bytesReceived = new ArraySegment<byte>(new byte[1024]);
+                    //WebSocketReceiveResult result = await ws.ReceiveAsync(bytesReceived, CancellationToken.None);
+                    //var backMsg = Encoding.UTF8.GetString(bytesReceived.Array, 0, result.Count);
+                    //1Console.WriteLine(backMsg);
+                    //Console.WriteLine(Encoding.UTF8.GetString(bytesReceived.Array, 0, result.Count));
+
+                }
+            }
+        }
+
     }
 }
